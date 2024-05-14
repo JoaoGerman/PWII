677146f0459f484f174be4e6d5bade0c4dc74943
Germano <124100799+JoaoGerman@users.noreply.github.com>
2024-05-14 13:35:25 -0300

    Update array.js

--- lacoderepeticao/while/js/array.js
+++ lacoderepeticao/while/js/array.js
@@ -6,7 +6,7 @@ var impar=0
 
 //esse while vai verificar cada valor do vetor e separar se é par ou impar
 while(i<vet.length){
-    if(vet[i]%2){
+    if(vet[i]%2==0){
         par=par+vet[i];
     }else{
         impar=impar+vet[i]
